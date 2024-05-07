70bca2107e09542ed5d86dc053481f14d75b3a50
kuri0317 <20cristy00@gmail.com>
2024-05-08 00:12:27 +0200

    modificato step 2

--- step_B.py
+++ step_B.py
@@ -19,57 +19,85 @@ for the position of the barycenter by scaling appropriately the associated joini
 sizes between the matching features).
 '''
 
-scene_2 = ['m1.png', 'm2.png', 'scenes/m3.png', 'm4.png', 'm5.png']
-product_2 = ['0.jpg', '1.jpg', '11.jpg', '19.jpg', '24.jpg', '26.jpg', '25.jpg']
-#scene_2 = ['object_detection_project/scenes/m1.png', 'object_detection_project/scenes/m2.png', 'object_detection_project/scenes/m3.png', 'object_detection_project/scenes/m4.png', 'object_detection_project/scenes/m5.png']
-#product_2 = ['object_detection_project/models/0.jpg', 'object_detection_project/models/1.jpg', 'object_detection_project/models/11.jpg', 'object_detection_project/models/19.jpg', 'object_detection_project/models/24.jpg', 'object_detection_project/models/26.jpg', 'object_detection_project/models/25.jpg']
+#scene_2 = ['m1.png', 'm2.png', 'scenes/m3.png', 'm4.png', 'm5.png']
+#product_2 = ['0.jpg', '1.jpg', '11.jpg', '19.jpg', '24.jpg', '26.jpg', '25.jpg']
+scene_2 = ['object_detection_project/scenes/m1.png', 'object_detection_project/scenes/m2.png', 'object_detection_project/scenes/m3.png', 'object_detection_project/scenes/m4.png', 'object_detection_project/scenes/m5.png']
+product_2 = ['object_detection_project/models/0.jpg', 'object_detection_project/models/1.jpg', 'object_detection_project/models/11.jpg', 'object_detection_project/models/19.jpg', 'object_detection_project/models/24.jpg', 'object_detection_project/models/26.jpg', 'object_detection_project/models/25.jpg']
 
-def find_instances(scene_2, product_2, threshold=0.75, min_matches=131):
+def find_instances(scene_2, product_2, threshold=0.75, min_matches=300, max_vect=10):
     
     sift = cv2.SIFT_create()
 
-    scene = cv2.imread(scene_2, cv2.IMREAD_GRAYSCALE)
-    keypoints_scene, descriptors_scene = sift.detectAndCompute(scene, None)
-
     count = {}
 
+    for scene in scene_2:
 
-    for prod_img in product_2:
-        product = cv2.imread(prod_img, cv2.IMREAD_GRAYSCALE)
+        scene_img = cv2.imread(scene, cv2.IMREAD_GRAYSCALE)
+        keypoints_scene, descriptors_scene = sift.detectAndCompute(scene_img, None)
 
-        keypoints_prod, descriptors_prod = sift.detectAndCompute(product, None)
-        
-        # Matcher descrittori metodo FLANN
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-        search_params = dict(checks=300)
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        scene_count ={}
+    
+        for prod_img in product_2:
+            
+            product = cv2.imread(prod_img, cv2.IMREAD_GRAYSCALE)
+            keypoints_prod, descriptors_prod = sift.detectAndCompute(product, None)
+            
+            # Matcher metodo FLANN
+            #CONTROLLA
+            FLANN_INDEX_KDTREE = 1
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=7)
+            search_params = dict(checks=50)
+            flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-        matches = flann.knnMatch(descriptors_prod, descriptors_scene, k=2)
-            #creare vettori
-            #vectors_prod = np.array([keypoint.pt for keypoint in keypoints_prod])
-            #vectors_scene = np.array([keypoint.pt for keypoint in keypoints_scene])
+            matches = flann.knnMatch(descriptors_prod, descriptors_scene, k=2)
+
+            good_matches = []
+            for m, n in matches:
+                if m.distance < threshold * n.distance:
+                    good_matches.append(m)
+            
+            if len(good_matches) >= min_matches:
+
+                centroid = np.mean([keypoint.pt for keypoint in keypoints_prod], axis=0)
+                edge_vectors = [keypoint.pt -centroid for keypoint in keypoints_prod]
+
+                if len(edge_vectors)> max_vect : #controllare che non si stampino troppi vettori
+                    edge_vectors= edge_vectors[:max_vect]
+
+                #contiamo le istanze
+                    
+                if prod_img in scene_count:
+                    scene_count[prod_img]['n_instance'] += 1
+
+                else:
         
-        good_matches = []
-        for m, n in matches:
-            if m.distance < threshold * n.distance:
-                good_matches.append(m)
-        
-        if len(good_matches) >= min_matches:
-            count[product_2] = len(good_matches)
+                    scene_count[prod_img]={
+                        'n_instance': 1,
+                        'centroid' : centroid,
+                        'edge_vectors' : edge_vectors
+                    }
+
+        count [scene]= scene_count
     
     return count    
 
-for scenes in scene_2:
-    print(f"scena: {scenes}")
 
-    count = find_instances(scenes, product_2)
 
-    for product_2,  instances in count.items():
-        print(f"Prodotto: {product_2}, Istanze trovate: {instances}")
-    
-    print("") 
-        #-----------------------------
+count = find_instances(scene_2,product_2, max_vect=10)
+
+for scene, scene_count in count.items():
+    print(f"Scena: {scene}")
+
+    for prod_img, c in scene_count.items():
+        print(f"Prodotto: {prod_img}")
+        print(f"Istanze trovate: {c['n_instance']}")
+        print(f"Baricentro del prodotto: {c['centroid']}")
+        print(f"Vettori degli edge al baricentro ( {len(c['edge_vectors'])} max vettori)")
+        for vector in c['edge_vectors']:
+            print(vector)
+        print("")  # Stampa una riga vuota per separare le informazioni dei prodotti
+    print("")  # Stampa una riga vuota per separare le informazioni delle scene
+
 
 '''
         def match_and_vote(scene_image, model_image_centroid, model_image_vectors):
@@ -134,4 +162,4 @@ for scenes in scene_2:
         cv2.destroyAllWindows()
 
 '''
-#----------------------------------------
+#---------------------------------------
