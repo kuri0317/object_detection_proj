0293b7f2661e61b87e70a7a374a7c9fe7c63f30e
kuri0317 <20cristy00@gmail.com>
2024-05-11 13:26:48 +0200

    cambiato punto 2

--- step_B.py
+++ step_B.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 from utils import getModelKeypointsDescriptors
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+
 '''
 #STEP 2  
 STEP 2 Multiple Instance Detection:
@@ -22,17 +25,15 @@ sizes between the matching features).
 scene_2 = ['object_detection_project/scenes/m1.png', 'object_detection_project/scenes/m2.png', 'object_detection_project/scenes/m3.png', 'object_detection_project/scenes/m4.png', 'object_detection_project/scenes/m5.png']
 product_2 = ['object_detection_project/models/0.jpg', 'object_detection_project/models/1.jpg', 'object_detection_project/models/11.jpg', 'object_detection_project/models/19.jpg', 'object_detection_project/models/24.jpg', 'object_detection_project/models/26.jpg', 'object_detection_project/models/25.jpg']
 
-def find_instances(scene_2, product_2, threshold=0.75, min_matches=300, max_vect=10):
+def find_instances(scene_2, product_2, threshold=0.75, min_matches=150, max_vect=5):
     
     sift = cv2.SIFT_create()
-
     count = {}
 
     for scene in scene_2:
 
         scene_img = cv2.imread(scene, cv2.IMREAD_GRAYSCALE)
         keypoints_scene, descriptors_scene = sift.detectAndCompute(scene_img, None)
-
         scene_count ={}
     
         for prod_img in product_2:
@@ -41,9 +42,8 @@ def find_instances(scene_2, product_2, threshold=0.75, min_matches=300, max_vect
             keypoints_prod, descriptors_prod = sift.detectAndCompute(product, None)
             
             # Matcher metodo FLANN
-            #CONTROLLA
             FLANN_INDEX_KDTREE = 1
-            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=7)
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
             search_params = dict(checks=50)
             flann = cv2.FlannBasedMatcher(index_params, search_params)
 
@@ -53,36 +53,75 @@ def find_instances(scene_2, product_2, threshold=0.75, min_matches=300, max_vect
             for m, n in matches:
                 if m.distance < threshold * n.distance:
                     good_matches.append(m)
-            
+        
             if len(good_matches) >= min_matches:
 
                 centroid = np.mean([keypoint.pt for keypoint in keypoints_prod], axis=0)
                 edge_vectors = [keypoint.pt -centroid for keypoint in keypoints_prod]
-                img_matches = cv2.drawMatches(product,keypoints_prod, scene_img, keypoints_scene, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-                cv2.imshow('Matches', img_matches)
-                cv2.waitKey(0)
+                scene_centroid = np.mean([keypoint.pt for keypoint in keypoints_scene], axis=0)
+                scene_vectors = [keypoint.pt -centroid for keypoint in keypoints_scene] #calcolo vettori e baricentri
+               
+                #ght
+                ght= cv2.createGeneralizedHoughBallard()
+                ght.setTemplate(product)
+                detections = ght.detect(scene_img)
+              
+                if detections is not None:
 
-                if len(edge_vectors)> max_vect : #controllare che non si stampino troppi vettori
-                    edge_vectors= edge_vectors[:max_vect]
+                    #serve accumulatore per voti? conviene fare funzione a parte?
+                    votes = np.zeros_like( centroid )
 
-                #contiamo le istanze
+                    for q_vec in edge_vectors:
+                        for m_vec in scene_vectors:
+                            # Calcola la trasformazione scalando il vettore modello in base al vettore di query
+                            scale_factor = np.linalg.norm(q_vec) / np.linalg.norm(m_vec)
+                            transformed_vector = m_vec * scale_factor
+                            
+                            # Calcola la posizione del baricentro traslato e vota
+                            vote_position = scene_centroid + transformed_vector 
+                            votes += vote_position
+                            #vedi dove stampare i voti
+
+                    scene_with_instances= cv2.cvtColor(scene_img, cv2.COLOR_GRAY2BGR)
+                    for detection in detections:
+                        for point in detection['pos']:
+                            x, y = int(point[0]), int(point[1])
+                            w, h = product.shape[::-1]
+                            cv2.rectangle(scene_with_instances, (x, y), (x + w, y + h), (0, 255, 0), 2) #disegniamo i riquadri
                     
-                if prod_img in scene_count:
-                    scene_count[prod_img]['n_instance'] += 1
+                    cv2.imshow('Scene with instances', scene_with_instances)
+                    cv2.waitKey(0)
 
                 else:
-        
-                    scene_count[prod_img]={
-                        'n_instance': 1,
-                        'centroid' : centroid,
-                        'edge_vectors' : edge_vectors
-                    }
+                    print ("rilevazione non valida")
 
+                if prod_img in scene_count:
+                    scene_count[prod_img]['n_instance'] += 1
+                else:
+                    scene_count[prod_img] = {
+                        'n_instance': 1,
+                        'centroid': centroid,
+                        'edge_vectors': edge_vectors
+                    }
+           '''
+            # Disegna
+            scene_centroids = product.copy()
+            cv2.circle(scene_centroids, tuple(map(int, centroid)), 10, (0, 255, 0), -1)
+                
+            cv2.imshow('scena con baricentri e vettori', scene_centroids)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+            if len(edge_vectors)> max_vect : #controllare che non si stampino troppi vettori
+                edge_vectors= edge_vectors[:max_vect]'''
+                
         count [scene]= scene_count
     
+    cv2.destroyAllWindows()
     return count    
 
-count = find_instances(scene_2,product_2, max_vect=10)
+count = find_instances(scene_2,product_2,threshold=0.75, min_matches=150, max_vect=5)
+
 
 for scene, scene_count in count.items():
     print(f"Scena: {scene}")
@@ -97,68 +136,3 @@ for scene, scene_count in count.items():
         print("")  # Stampa una riga vuota per separare le informazioni dei prodotti
     print("")  # Stampa una riga vuota per separare le informazioni delle scene
 
-
-'''
-        def match_and_vote(scene_image, model_image_centroid, model_image_vectors):
-            sift = cv2.SIFT_create()
-            keypoints, descriptors = sift.detectAndCompute(scene_image, None)
-            
-            votes = np.zeros_like(model_image_centroid)
-            
-            bf = cv2.BFMatcher()
-            matches = bf.knnMatch(descriptors, descriptors_model, k=2)
-
-            for m, n in matches:
-                if m.distance < 0.75 * n.distance:
-                    scene_point = keypoints[m.trainIdx].pt
-                    model_point = model_image_vectors[m.queryIdx]
-                    
-                    size_ratio = m.distance / n.distance
-                
-                    scaled_vector = (model_point - model_image_centroid) * size_ratio
-                    
-                    votes += scaled_vector
-            
-            return votes
-    
-        
-        model_centroid = np.mean(vectors_model, axis=0)
-        scene_centroids = [] # lista per le posizioni
-
-        bf = cv2.BFMatcher()
-
-        matches = bf.knnMatch(descriptors_model, descriptors_scene, k=2)
-
-        good_matches = [] 
-
-        for m, n in matches:
-            if m.distance < 0.75 * n.distance:
-                good_matches.append(m)
-                
-        for m in good_matches:
-            scene_centroid_estimate = vectors_scene[m.trainIdx] + model_centroid - vectors_model[m.queryIdx]
-            scene_centroids.append(scene_centroid_estimate)
-
-        #clustering oer raggruppare posizioni
-        X = np.array(scene_centroids)
-        db = DBSCAN(eps=10, min_samples=5).fit(X)
-        labels = db.labels_
-
-        n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
-        print("Numero istanze multiple trovate:", n_clusters)
-
-        # Disegna
-        scene_img_with_centroids = scene.copy()
-        for i in range(n_clusters):
-            cluster_points = X[labels == i]
-            centroid = np.mean(cluster_points, axis=0)
-            cv2.circle(scene_img_with_centroids, tuple(map(int, centroid)), 10, (0, 255, 0), -1)
-            for point in cluster_points:
-                cv2.arrowedLine(scene_img_with_centroids, tuple(map(int, point)), tuple(map(int, centroid)), (0, 0, 255), 2)
-
-        cv2.imshow('scena con baricentri e vettori', scene_img_with_centroids)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-'''
-#---------------------------------------
